65d64829cad79127ee8657f9f97e082c5230ec21
root <sharful09@gmail.com>
2024-02-29 11:33:35 +0600

    fix: reset database
    Some containers are named, it is easy for identify.
    change the port for bahisdb
    set timezone to Asia/Dhaka

--- serve/pgdb/resetdb.sh
+++ serve/pgdb/resetdb.sh
@@ -2,5 +2,5 @@
 #run with your current database you want o switch to:
 #whichdb=coredb-29-03-2022.sql ./resetdb.sh
 set -e
-psql -U postgres -h localhost -p 5432 -f init.sql --set ON_ERROR_STOP
-psql -U postgres -h localhost -p 5432 -d coredb -f $whichdb --set ON_ERROR_STOP
+psql -U postgres -h localhost -p 5444 -f init.sql --set ON_ERROR_STOP
+psql -U postgres -h localhost -p 5444 -d coredb -f $whichdb --set ON_ERROR_STOP
