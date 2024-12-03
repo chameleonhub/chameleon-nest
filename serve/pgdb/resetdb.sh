792f2d705be312a26d35a7dcde3f61279c5ee631
Sharful <sharful09@gmail.com>
2024-12-03 15:55:55 +0600

    fix: remove and add submodule

--- serve/pgdb/resetdb.sh
+++
@@ -1,6 +0,0 @@
-#Usage:
-#run with your current database you want o switch to:
-#whichdb=coredb-29-03-2022.sql ./resetdb.sh
-set -e
-psql -U postgres -h localhost -p 5444 -f init.sql --set ON_ERROR_STOP
-psql -U postgres -h localhost -p 5444 -d coredb -f $whichdb --set ON_ERROR_STOP
