3757061a13ece8c51e9e0a57a297eb5c9719da60
Sharful <sharful09@gmail.com>
2024-12-09 15:30:40 +0600

    fix: remove comment

--- chameleon-kobo/kpi/settings/urls.py
+++ chameleon-kobo/kpi/settings/urls.py
@@ -2,6 +2,5 @@ from django.urls import include, path
 from kobo.urls import *
 
 urlpatterns += [
-    # path(r'desk/', include("kobo.apps.desk.urls")),
     path(r'api/v2/utils/', include("kobo.apps.chameleon.utils.urls")),
 ]
